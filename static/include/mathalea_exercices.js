9cb004a6d7f30a04175577ca6572fc4576d91d15
Jean-Claude Lhote <lhotejc@gmail.com>
2020-01-18 00:46:16 +0100

    refonte totale Trouver l'inverse.

--- static/include/mathalea_exercices.js
+++ static/include/mathalea_exercices.js
@@ -2762,61 +2762,95 @@ function Exercice_Trouver_l_inverse(){
 		this.liste_questions = []; // Liste de questions
 		this.liste_corrections = []; // Liste de questions corrigées
 		let type_de_questions_disponibles
+		let liste_entiers=[[1,1],[2,0.5],[3,0],[4,0.25],[5,0.2],[6,0],[7,0],[8,0.125],[9,0],[10,0.1],[11,0],[12,0],[13,0],[14,0],[15,0],[20,0.05],[50,0.02],[100,0.01]]
+		// [n,0] si l'inverse de n n'est pas décimal [n,1/n] si il est décimal.
+		let liste_decimaux=[[0.1,10,1],[0.2,5,1],[0.3,10,3],[0.4,10,4],[0.5,2,1],[0.6,10,6],[0.75,100,75],[0.8,10,8],[1.2,10,12],[1.5,10,15],[2.5,10,25],[3.5,10,35],[4.8,10,48],[7.5,10,75]]
+		// [x,n,d] n/d = inverse de x fraction à réduire si besoin ... d=1 si l'inverse de x est entier. 
+		let liste_fractions=[[3,4,false],[5,2,true],[4,5,true],[5,7,true],[7,3,false],[16,6,true],[12,18,true],[9,4,false],[4,6,true],[8,7,true],[5,9,true],[9,7,false],[13,6,false],[7,2,false]]
+		// [n,d,bol] inverse d/n à simplifier si besoin. si bol = true, alors d/n est décimal.
 		let liste_couples_d_inverses
 		let couples_d_inverses
 		if (this.sup==4) {type_de_questions_disponibles = [1,1,2,2,3]} // nombre entier,fraction,décimal]
 		else {type_de_questions_disponibles = [parseInt(this.sup)]}
 		let liste_type_de_questions = combinaison_listes(type_de_questions_disponibles,this.nb_questions)
-		for (let i = 0, nombre_choisi, nombre_inverse, texte, texte_corr, type_de_questions, cpt=0; i < this.nb_questions&&cpt<50;) {
+		for (let i = 0, nombre_choisi, nombre_inverse, nombre_inverse_num, nombre_inverse_den, texte, texte_corr, type_de_questions, cpt=0; i < this.nb_questions&&cpt<50;) {
 			type_de_questions = liste_type_de_questions[i];
 			switch (type_de_questions){
-				case 1 :
-					liste_couples_d_inverses = [['1','1'],['2','0,5'],['3',tex_fraction(1,3)],['4','0,25'],['5','0,2'],['6',tex_fraction(1,6)],['8','0,125'],['10','0,1'],['20','0,05'],['16','0,0625'],[7,tex_fraction(1,7)]]
-					couples_d_inverses = choice(liste_couples_d_inverses)
-					if (choice([true,false])) {
-						nombre_choisi = couples_d_inverses[0];
-						nombre_inverse = couples_d_inverses[1];
-						texte_corr = `L\'inverse de $${nombre_choisi}$ est $${nombre_inverse} \\:$ car $\\: ${nombre_choisi}   \\times   ${nombre_inverse} =  1$.`
-					} else {
-						nombre_choisi = '-'+couples_d_inverses[0];
-						nombre_inverse = '-'+couples_d_inverses[1];
-						texte_corr = `L'inverse de $${nombre_choisi}$ est $${nombre_inverse} \\:$`
-						nombre_inverse = '(-'+couples_d_inverses[1]+')';
-						texte_corr+=` car $\\: ${nombre_choisi}  \\times  ${nombre_inverse}  =  1$.`
+				case 1 : //inverse d'entier
+					couples_d_inverses = choice(liste_entiers)
+					nombre_choisi = couples_d_inverses[0];
+					nombre_inverse = couples_d_inverses[1];
+					if (choice([true,false])) { // nombre entier positif
+						if (nombre_inverse!=0) {  //inverse décimal
+							texte_corr = `L\'inverse de $${nombre_choisi}$ est $${tex_nombrec(nombre_inverse)} \\:$ car $\\: ${nombre_choisi}   \\times   ${tex_nombrec(nombre_inverse)} =  1$.`
+						}
+						else {  //inverse non décimal
+							texte_corr = `L\'inverse de $${nombre_choisi}$ est $${tex_fraction(1,nombre_choisi)} \\:$ car $\\: ${nombre_choisi}   \\times   ${tex_fraction(1,nombre_choisi)} =  1$.`
+						}
+						} else { //nombre entier négatif
+							nombre_choisi=-nombre_choisi
+							if (nombre_inverse!=0) { //inverse décimal
+								texte_corr = `L'inverse de $${nombre_choisi}$ est $${tex_nombrec(-nombre_inverse)} \\:$`
+								texte_corr+=` car $\\: ${nombre_choisi}  \\times  (-${tex_nombrec(nombre_inverse)})  =  1$.`
+							}
+							else {  //inverse non décimal
+								texte_corr = `L\'inverse de $${nombre_choisi}$ est $-${tex_fraction(1,-nombre_choisi)} \\:$ car $\\: ${nombre_choisi}   \\times   (-${tex_fraction(1,-nombre_choisi)}) =  1$.`
+							}
 					}
+					texte = `Quel est l'inverse de $${tex_nombrec(nombre_choisi)}$ ?`;
 				break
 				case 2 :
-					liste_couples_d_inverses = [[tex_fraction(2,3),'1,5'],[tex_fraction(4,5),'1,25'],[tex_fraction(5,6),'1,2'],[tex_fraction(7,6),tex_fraction(6,7)],[tex_fraction(8,7),'0,875'],[tex_fraction(9,8),tex_fraction(8,9)]]
-					couples_d_inverses = choice(liste_couples_d_inverses)
-					if (choice([true,false])) {
-						nombre_choisi = couples_d_inverses[0];
-						nombre_inverse = couples_d_inverses[1];
-						texte_corr = `L'inverse de $${nombre_choisi}$ est $${nombre_inverse} \\:$ car $\\: ${nombre_choisi}   \\times   ${nombre_inverse} =  1$.`
-					} else {
-						nombre_choisi = '-'+couples_d_inverses[0];
-						nombre_inverse = '-'+couples_d_inverses[1];
-						texte_corr = `L'inverse de $${nombre_choisi}$ est $${nombre_inverse} \\:$`
-						nombre_inverse = '(-'+couples_d_inverses[1]+')';
-						texte_corr+=` car $\\: ${nombre_choisi}  \\times  ${nombre_inverse}  =  1$.`
+					couples_d_inverses = choice(liste_decimaux)
+					nombre_choisi = couples_d_inverses[0];
+					nombre_inverse_num = couples_d_inverses[1];
+					nombre_inverse_den = couples_d_inverses[2];
+					if (choice([true,false])) { // nombre positif
+						if (pgcd(nombre_inverse_num,nombre_inverse_den)==1) {  //non simplifiable après inversion
+							texte_corr = `Comme $${tex_nombrec(nombre_choisi)}=${tex_fraction(nombre_inverse_den,nombre_inverse_num)}$, l'inverse de $${tex_nombrec(nombre_choisi)}$ est $${tex_fraction(nombre_inverse_num,nombre_inverse_den)} \\:$ car $\\: ${tex_fraction(nombre_inverse_den,nombre_inverse_num)}   \\times   ${tex_fraction(nombre_inverse_num,nombre_inverse_den)} =  1$.`
+						}
+						else {  // à simplifier après inversion
+							texte_corr = `Comme $${tex_nombrec(nombre_choisi)}=${tex_fraction(nombre_inverse_den,nombre_inverse_num)}=${tex_fraction_reduite(nombre_inverse_den,nombre_inverse_num)}$, l'inverse de $${tex_nombrec(nombre_choisi)}$ est $${tex_fraction_reduite(nombre_inverse_num,nombre_inverse_den)} \\:$ car $\\: ${tex_fraction_reduite(nombre_inverse_den,nombre_inverse_num)}  \\times   ${tex_fraction_reduite(nombre_inverse_num,nombre_inverse_den)} =  1$.`	
+						}
+							
+					} else { // nombre négatif
+						nombre_choisi=-nombre_choisi
+						if (pgcd(nombre_inverse_num,nombre_inverse_den)==1) {  //non simplifiable après inversion
+							texte_corr = `L'inverse de $${tex_nombrec(nombre_choisi)}$ est $-${tex_fraction(nombre_inverse_num,nombre_inverse_den)} \\:$ car $\\: ${tex_nombrec(nombre_choisi)}   \\times   (-${tex_fraction(nombre_inverse_num,nombre_inverse_den)}) =  1$.`
+							texte_corr = `Comme $${tex_nombrec(nombre_choisi)}=-${tex_fraction(nombre_inverse_den,nombre_inverse_num)}$, l'inverse de $${tex_nombrec(nombre_choisi)}$ est $-${tex_fraction(nombre_inverse_num,nombre_inverse_den)} \\:$ car $\\: -${tex_fraction(nombre_inverse_den,nombre_inverse_num)}   \\times  (- ${tex_fraction(nombre_inverse_num,nombre_inverse_den)}) =  1$.`
+						
+						}
+						else {  // à simplifier après inversion
+							texte_corr = `Comme $${tex_nombrec(nombre_choisi)}=-${tex_fraction(nombre_inverse_den,nombre_inverse_num)}=-${tex_fraction_reduite(nombre_inverse_den,nombre_inverse_num)}$, l'inverse de $${tex_nombrec(nombre_choisi)}$ est $-${tex_fraction_reduite(nombre_inverse_num,nombre_inverse_den)} \\:$ car $\\: -${tex_fraction_reduite(nombre_inverse_den,nombre_inverse_num)}  \\times  (- ${tex_fraction_reduite(nombre_inverse_num,nombre_inverse_den)} )=  1$.`	
 					}
+					}
+					texte = `Quel est l'inverse de $${tex_nombrec(nombre_choisi)}$ ?`;
 				break	
 				case 3 :
-					liste_couples_d_inverses = [['0,6',tex_fraction(5,3)],['0,75',tex_fraction(4,3)],['0,1','10'],['0,4','2,5'],['5,5',tex_fraction(2,11)],['12,5','0,08'],['7,5',tex_fraction(2,15)]]
-					couples_d_inverses = choice(liste_couples_d_inverses)
-					if (choice([true,false])) {
-						nombre_choisi = couples_d_inverses[0];
-						nombre_inverse = couples_d_inverses[1];
-						texte_corr = `L'inverse de $${nombre_choisi}$ est $${nombre_inverse} \\:$ car $\\: ${nombre_choisi}   \\times   ${nombre_inverse} =  1$.`
-					} else {
-						nombre_choisi = '-'+couples_d_inverses[0];
-						nombre_inverse = '-'+couples_d_inverses[1];
-						texte_corr = `L'inverse de $${nombre_choisi}$ est $${nombre_inverse} \\:$`
-						nombre_inverse = '(-'+couples_d_inverses[1]+')';
-						texte_corr+=` car $\\: ${nombre_choisi}  \\times  ${nombre_inverse}  =  1$.`
+					couples_d_inverses = choice(liste_fractions)
+					nombre_inverse_num = couples_d_inverses[0];
+					nombre_inverse_den = couples_d_inverses[1];
+					if (choice([true,false])) {  // fraction positive
+						if (couples_d_inverses[2]==true) {  // inverse décimal
+							texte_corr = `L'inverse de $${tex_fraction(nombre_inverse_num,nombre_inverse_den)}$ est $${tex_fraction(nombre_inverse_den,nombre_inverse_num)}=${tex_nombrec(nombre_inverse_den/nombre_inverse_num)} \\:$ car $\\: ${tex_fraction(nombre_inverse_num,nombre_inverse_den)}   \\times   ${tex_fraction(nombre_inverse_den,nombre_inverse_num)} =  1$.`
+						}
+						else {   // inverse non décimal
+							texte_corr = `L'inverse de $${tex_fraction(nombre_inverse_num,nombre_inverse_den)}$ est $${tex_fraction(nombre_inverse_den,nombre_inverse_num)} \\:$ car $\\: ${tex_fraction(nombre_inverse_num,nombre_inverse_den)}   \\times   ${tex_fraction(nombre_inverse_den,nombre_inverse_num)} =  1$.`
+						}
+						texte = `Quel est l'inverse de $${tex_fraction(nombre_inverse_num,nombre_inverse_den)}$ ?`;
+					} 
+					else {  // fraction négative
+						if (couples_d_inverses[2]==true) {  // inverse décimal
+							texte_corr = `L'inverse de $-${tex_fraction(nombre_inverse_num,nombre_inverse_den)}$ est $-${tex_fraction(nombre_inverse_den,nombre_inverse_num)}=-${tex_nombrec(nombre_inverse_den/nombre_inverse_num)} \\:$ car $\\: -${tex_fraction(nombre_inverse_num,nombre_inverse_den)}   \\times  (- ${tex_fraction(nombre_inverse_den,nombre_inverse_num)}) =  1$.`
+						}
+						else {   // inverse non décimal
+							texte_corr = `L'inverse de $-${tex_fraction(nombre_inverse_num,nombre_inverse_den)}$ est $-${tex_fraction(nombre_inverse_den,nombre_inverse_num)} \\:$ car $\\: -${tex_fraction(nombre_inverse_num,nombre_inverse_den)}   \\times  (- ${tex_fraction(nombre_inverse_den,nombre_inverse_num)} )=  1$.`
+						}
+						texte = `Quel est l'inverse de $-${tex_fraction(nombre_inverse_num,nombre_inverse_den)}$ ?`;
 					}
+					
 				break	
 			}
-			texte = `Quel est l'inverse de $${nombre_choisi}$ ?`;
+			
 			if (this.liste_questions.indexOf(texte)==-1){ // Si la question n'a jamais été posée, on en créé une autre
 				this.liste_questions.push(texte);
 				this.liste_corrections.push(texte_corr);
